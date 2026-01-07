bf7fb1482344f116220d1be28633328eb3216906
BoopathiC <cboopathipnr@gmail.com>
2026-01-07 22:18:22 +0530

    Add package.json and dependencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import apiRoutes from '../routes/index.js';
+import apiRoutes from './meditrack-backend/routes/index.js';
 
 dotenv.config();
 
